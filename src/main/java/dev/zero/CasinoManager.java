7e4512f9732af2df303328ff890b74ed1f83c57c
Zero-Ramon <ramon.lopes5932@gmail.com>
2025-07-24 08:27:01 -0300

    Update CasinoManager.java

--- src/main/java/dev/zero/CasinoManager.java
+++ src/main/java/dev/zero/CasinoManager.java
@@ -1,15 +1,15 @@
-// CasinoManager.java
+
 package dev.zero;
 
 public class CasinoManager {
     private static String owner = null;
 
-    /** Define o proprietário do cassino */
+
     public static void setOwner(String ownerName) {
         owner = ownerName;
     }
 
-    /** Retorna o proprietário ou "Nenhum" se não houver */
+
     public static String getOwner() {
         return (owner != null && !owner.isBlank()) ? owner : "Nenhum";
     }
